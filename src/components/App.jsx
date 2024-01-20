fb277acaaf6a406e7c2ba75c5cb7389acc50f7a3
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-01-20 09:20:52 +0200

    clear code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,7 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-
 import { ContactForm, Filter, ContactList } from 'components';
 import css from './ContactsForm.module.css';
 
 export const App = () => {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const stringifiedContacts = localStorage.getItem('contacts');
-  //   const storedContacts = JSON.parse(stringifiedContacts) || [];
-
-  //   const action = {
-  //     type: 'contacts/setContacts',
-  //     payload: storedContacts,
-  //   };
-  //   dispatch(action);
-  // }, [dispatch]);
-
   return (
     <div className={css.form_container}>
       <h1>Phonebook</h1>
